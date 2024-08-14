5adc28be0a0617138fcacb10dc620aabc9620de3
jesus73514145 <jesus_soria@usmp.pe>
2024-08-14 10:16:45 -0500

    README
    Co-authored-by: AlbertoSoria1998 <albertosoriasoria74@gmail.com>

--- src/Home.tsx
+++ src/Home.tsx
@@ -37,6 +37,7 @@ export default function Home() {
   };
   return (
     <>
+    <p>MICROFRONT 2</p>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
